0872f74be7376b5cdfccf3b4d5e862dd02097c78
João Vitor Araújo Dos Santos <70721670+JoaoV1821@users.noreply.github.com>
2021-01-15 12:12:48 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,32 @@
+import wikipedia as wiki
+
+wiki.set_lang('pt')
+
+pesquisa = wiki.summary('Dom Casmurro')
+
+alfabeto = str('abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ ')
+pos = []
+
+Mode_Encrypt = True
+Mode_Decrypt = False
+
+print(len(alfabeto))
+palavra = str(pesquisa.replace(" ", "*"))
+
+
+def cifra(msg, chave, modo):
+
+    if modo:
+        for p in msg:
+            pos.append(alfabeto.find(p))
+        for n in pos:
+            print(f'{alfabeto[n+chave]}', end='')
+
+    if not modo:
+        for p in msg:
+            pos.append(alfabeto.find(p))
+        for n in pos:
+            print(f'{alfabeto[n-chave]}', end='')
+
+
+cifra(palavra, 7, Mode_Decrypt)
